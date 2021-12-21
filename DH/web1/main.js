3dd84d070c66726ab0062916a3206894fa571be5
YiKM <chesskenshin@gmail.com>
2021-12-22 00:02:49 +0900

    2021.12.22
    MariaDB 설치

--- DH/web1/main.js
+++ DH/web1/main.js
@@ -2,35 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-function templateHTML(title, list, body, control) {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                ${control}
-                <p>${body}</p>
-            </body>
-    </html>
-    `;
-}
-function templateList(filelist){
-    var list = '<ul>';
-    var i = 0;
-    while(i < filelist.length){
-        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        i = i + 1;
-    }
-    list = list + '</ul>';
-    return list;
-}
-
+var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -41,20 +15,23 @@ var app = http.createServer(function(request,response){
                 fs.readdir('./data', function(error, filelist){
                     var title = 'Welcome';
                     var description = 'Hello, nodejs';
-                    var list = templateList(filelist);
-                    var template = templateHTML(title, list, `<h2>${title}<h2><p>${description}</p>`,`<a href="/create">create</a>`
+                    var list = template.list(filelist);
+                    var html = template.HTML(title, list, `<h2>${title}<h2><p>${description}</p>`,`<a href="/create">create</a>`
                     ); 
                 response.writeHead(200);
                 response.end(template);                             
                 });      
         }else{
             fs.readdir('./data', function(error, filelist){
-                fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-                    var title = queryData.id;                    
-                    var list = templateList(filelist);
-                    var template = templateHTML(title, list, `<h2>${title}<h2><p>${description}</p>`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>
+                var filteredID = path.parse(queryData.id).base;
+                fs.readFile(`data/${filteredID}`, 'utf-8', function(err, description){
+                    var title = queryData.id;
+                    var sanitizedTitle = sanitizeHtml(title);
+                    var sanitizedDescription = sanitizeHtml(description, {allowedTags:['h1']});                    
+                    var list = template.list(filelist);
+                    var html = template.HTML(sanitizedTitle, list, `<h2>${sanitizedTitle}<h2><p>${sanitizedDescription}</p>`,`<a href="/create">create</a> <a href="/update?id=${sanitizedTitle}">update</a>
                         <form action="delete_process" method="post" onsubmit="return confirm('정말로 삭제하겠습니까?');">
-                            <input type="hidden" name="id" value="${title}">
+                            <input type="hidden" name="id" value="${sanitizedTitle}">
                             <input type="submit" value="delete">
                         </form>`
                     ); 
@@ -66,8 +43,8 @@ var app = http.createServer(function(request,response){
 }else if(pathname === '/create'){
     fs.readdir('./data', function(error, filelist){
         var title ='임대 생성';
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `
+        var list = template.list(filelist);
+        var html = template.HTML(title, list, `
             <form action="/create_process" method="post">
                 <p><input type="text" name="title" placeholder="title"></p>
                 <p>
@@ -98,10 +75,11 @@ else if(pathname === '/create_process'){
 }
 else if(pathname === '/update'){
     fs.readdir('./data', function(error, filelist){
-        fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+        var filteredID = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredID}`, 'utf-8', function(err, description){
             var title = queryData.id;
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, 
+            var list = template.list(filelist);
+            var html = template.HTML(title, list, 
                 `<form action="/update_process" method="post">
                     <p><input type="hidden" name="id" value="${title}">
                     <p><input type="text" name="title" placeholder="title" value="${title}"></p>    
@@ -144,7 +122,8 @@ else if(pathname === '/delete_process'){
     request.on('end', function(){
         var post = qs.parse(body);
         var id = post.id;
-        fs.unlink(`data/${id}`, function(error){
+        var filteredID = path.parse(id).base;
+        fs.unlink(`data/${filteredID}`, function(error){
             response.writeHead(302, {Location: `/`});
             response.end();
         });
